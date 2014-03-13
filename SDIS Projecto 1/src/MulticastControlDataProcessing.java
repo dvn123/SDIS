43ad25ce4f51857848f9d95fb5080bbdef1638ae
dvn123 <diogovaznunes@gmail.com>
2014-03-13 08:33:35 +0000

    ll

---
+++ SDIS Projecto 1/src/MulticastControlDataProcessing.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Diogo on 12-03-2014.
+ */
+public class MulticastControlDataProcessing implements Runnable {
+    private String message;
+
+    private void check_message() {
+
+    }
+
+    public void run() {
+
+    }
+}
